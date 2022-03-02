7e0444d1caa9c29f757506b7b4bfd7965add5af7
ganesh23-alt <ganeshranabhat23@gmail.com>
2022-03-02 19:48:11 +0545

    deploy

---
+++ src/layout/AppHeader.vue
@@ -0,0 +1,89 @@
+<template>
+    <header class="header-global">
+        <base-nav class="navbar-main" transparent type="" effect="light" expand>
+            <router-link slot="brand" class="navbar-brand mr-lg-6" to="/">
+                <img src="img/brand/white.png" alt="logo">
+            </router-link>
+
+            <div class="row" slot="content-header" slot-scope="{closeMenu}">
+                <div class="col-6 collapse-brand">
+                    <a href="#">
+                        <img src="img/brand/white.png">
+                    </a>
+                </div>
+                <div class="col-6 collapse-close">
+                    <close-button @click="closeMenu"></close-button>
+                </div>
+            </div>
+
+            <ul class="navbar-nav navbar-nav-hover align-items-lg-center">
+               <!-- <base-dropdown class="nav-item" menu-classes="dropdown-menu-xl">
+                   Something Goes here
+                </base-dropdown> -->
+                <router-link to="/" class="dropdown-item">Home</router-link>
+                <base-dropdown tag="li" class="nav-item">
+                    <a slot="title" href="#" class="nav-link" data-toggle="dropdown" role="button">
+                        <span class="nav-link-inner--text">Services</span>
+                    </a>
+                    <router-link to="#" class="dropdown-item ">Web Design</router-link>
+                    <router-link to="#" class="dropdown-item">Digital Marketing</router-link>
+                    <router-link to="#" class="dropdown-item">Domain Registration</router-link>
+                </base-dropdown>
+                <router-link to="/profile" class="nav-link  mx-lg-2"> Teams</router-link>
+                <router-link to="/login" class="nav-link mx-lg-2">Login</router-link>
+                <router-link to="/register" class="nav-link mx-lg-2">Register</router-link>
+            </ul>
+            <ul class="navbar-nav align-items-lg-center ml-lg-auto">
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="https://www.facebook.com/geccodev" target="_blank" rel="noopener"
+                       data-toggle="tooltip" title="Like us on Facebook">
+                        <i class="fa fa-facebook-square"></i>
+                        <span class="nav-link-inner--text d-lg-none">Facebook</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="https://www.instagram.com/geccodev"
+                       target="_blank" rel="noopener" data-toggle="tooltip" title="Follow us on Instagram">
+                        <i class="fa fa-instagram"></i>
+                        <span class="nav-link-inner--text d-lg-none">Instagram</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="https://twitter.com/geccodev" target="_blank" rel="noopener"
+                       data-toggle="tooltip" title="Follow us on Twitter">
+                        <i class="fa fa-twitter-square"></i>
+                        <span class="nav-link-inner--text d-lg-none">Twitter</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-icon" href="https://github.com/ganesh23-alt"
+                       target="_blank" rel="noopener" data-toggle="tooltip" title="Star us on Github">
+                        <i class="fa fa-github"></i>
+                        <span class="nav-link-inner--text d-lg-none">Github</span>
+                    </a>
+                </li>
+                <li class="nav-item d-none d-lg-block ml-lg-4">
+                    <router-link to="/quotatiom" class="btn btn-neutral btn-icon">
+                        <span class="nav-link-inner--text">Request Quotation</span>
+                    </router-link>
+
+                </li>
+            </ul>
+        </base-nav>
+    </header>
+</template>
+<script>
+import BaseNav from "@/components/BaseNav";
+import BaseDropdown from "@/components/BaseDropdown";
+import CloseButton from "@/components/CloseButton";
+
+export default {
+  components: {
+    BaseNav,
+    CloseButton,
+    BaseDropdown
+  }
+};
+</script>
+<style>
+</style>
